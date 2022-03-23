e4815444c5f97d717672a8ae92ceccaccf576fa4
duyphan119 <duy.910.2015@gmail.com>
2022-03-23 10:26:22 +0700

    first commit

---
+++ demoAPI/Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using demoAPI.Models;
+
+namespace demoAPI.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        // GET: api/Weather
+        public IEnumerable<WeatherInfo> Get()
+        {
+            var weatherInfoList = new List<WeatherInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                var WeatherInfo = new WeatherInfo
+                {
+                    Location = $"Location {i}",
+                    Degree = i * 23 / 17,
+                    DateTime = DateTime.Now.ToUniversalTime()
+                };
+                weatherInfoList.Add(WeatherInfo);
+            }
+
+            return weatherInfoList;
+        }
+
+        // GETL: api/Weather/5
+        public WeatherInfo Get(int id)
+        {
+             return new WeatherInfo
+            {
+                Location = $"Location {id}",
+                Degree = id * 23 / 17,
+                DateTime = DateTime.Now.ToUniversalTime()
+            };
+        }
+
+    }
+}
